a27763ae58fbae5bb64b7af918c1118bf0c9cb52
EL1A5 <provinciaelias@gmail.com>
2022-04-06 15:45:18 -0500

    spring security roles users

--- src/main/java/com/departamento/Pid202201MiercolesGrupo05Application.java
+++ src/main/java/com/departamento/Pid202201MiercolesGrupo05Application.java
@@ -1,13 +1,32 @@
 package com.departamento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class Pid202201MiercolesGrupo05Application {
+public class Pid202201MiercolesGrupo05Application implements CommandLineRunner {
 
+	@Autowired
+	private BCryptPasswordEncoder passEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pid202201MiercolesGrupo05Application.class, args);
+	
+		
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String pass1="user";
+		String pass2="admin";
+		
+		System.out.println(passEncoder.encode(pass1));
+		System.out.println(passEncoder.encode(pass2));
+	}
+
+	
 
 }
--- src/main/java/com/departamento/entity/prueba1_provincia.java
+++ src/main/java/com/departamento/entity/prueba1_provincia.java
@@ -1,5 +1,14 @@
 package com.departamento.entity;
 
-public class prueba1_provincia {
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Table(name ="alumo")
+public class prueba1_provincia {
+	
+	private int edad;
+	
 }
