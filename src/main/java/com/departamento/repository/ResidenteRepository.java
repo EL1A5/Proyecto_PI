8dd7319b9b1138ab286c273d924ace1e62faa33c
AlexanderBern001 <bryan9112@hotmail.com>
2022-04-09 11:26:18 -0500

    Branch Bernuy- Residente

---
+++ src/main/java/com/departamento/repository/ResidenteRepository.java
@@ -0,0 +1,17 @@
+package com.departamento.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import com.departamento.entity.Residente;
+
+public interface ResidenteRepository extends JpaRepository<Residente, Integer>{
+	@Query("select e from residente e where e.dni = ?1")
+	public List<Residente> listaPorDni(String dni);
+	
+	@Query("select e from residente e where e.dni = ?1 and e.idresidente <> ?2")
+	public List<Residente> listaPorDniDiferenteSiMismo(String dni, int idresidente);
+
+	
+
+}
---
+++ src/main/java/com/departamento/service/ResidenteServiceImp.java
@@ -0,0 +1,42 @@
+package com.departamento.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.departamento.entity.Residente;
+import com.departamento.repository.ResidenteRepository;
+
+public class ResidenteServiceImp implements ResidenteService{
+	@Autowired
+	private ResidenteRepository repository;
+	
+	@Override
+	public List<Residente> listaResidente() {
+		return repository.findAll();
+	}
+	@Override
+	public Residente insertaActualizaResidente(Residente obj) {
+		return repository.save(obj);
+	}
+
+	@Override
+	public List<Residente> listaResidentePorDni(String dni) {
+		return repository.listaPorDni(dni);
+	}
+
+	@Override
+	public List<Residente> listaResidentePorDniDiferenteDelMismo(String dni, int idresidente) {
+		return repository.listaPorDniDiferenteSiMismo(dni, idresidente);
+	}
+
+	@Override
+	public Optional<Residente> listaResidentePorId(int idresidente) {
+		return repository.findById(idresidente);
+		}
+	@Override
+	public void eliminaResidentePorId(int idresidente) {
+		repository.deleteById(idresidente);
+	}
+
+}
