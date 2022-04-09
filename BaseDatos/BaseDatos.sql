f322fa4d337d5502345f7ba16840a0d318b0e8d7
AlexanderBern001 <bryan9112@hotmail.com>
2022-04-09 11:55:58 -0500

    bd inserts residente update

--- BaseDatos/BaseDatos.sql
+++ BaseDatos/BaseDatos.sql
@@ -129,3 +129,14 @@ SELECT u.username, u.password, u.enable FROM users u WHERE u.username = 'user';
 SELECT r.user_id, r.rolname FROM roles r inner join users u 
 on r.user_id=u.id where u.username='admin';
 
+
+insert into residente values
+(null,"Bryan Alexander","Bernuy Bravo",71434952,1,"Inquilino","2020-05-05",1);
+insert into residente values
+(null,"Marco Alberto","Vicenzi Gianetto",09621521,1,"Propietario","2020-05-05",1);
+insert into residente values
+(null,"Mariela Morena","Beltran Renitti",09621545,4,"Inquilino","2020-07-05",1);
+insert into residente values
+(null,"Julio Miguel","Benedetti Saldaña",05555444,3,"Propietario","2022-05-05",3);
+
+select * from residente;
