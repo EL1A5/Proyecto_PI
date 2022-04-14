a1dbafc2cf0caed3054f58ce7f0d7f331d76fe10
AlexanderBern001 <bryan9112@hotmail.com>
2022-04-14 18:23:29 -0500

    update

--- src/main/java/com/departamento/entity/Residente.java
+++ src/main/java/com/departamento/entity/Residente.java
@@ -25,6 +25,7 @@ public class Residente {
 	private String nombre;
 	private String apellidos;
 	private String dni;
+	private String correo;
 	private int mascotas;
 	private String estado;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
