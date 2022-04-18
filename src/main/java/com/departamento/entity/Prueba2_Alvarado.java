49d659c85f1e7ee29eba164ac1d4be311ddf13ed
EL1A5 <provinciaelias@gmail.com>
2022-04-18 12:00:48 -0500

    eliminando proyecto para uno nuevo

--- src/main/java/com/departamento/entity/Prueba2_Alvarado.java
+++
@@ -1,5 +0,0 @@
-package com.departamento.entity;
-
-public class Prueba2_Alvarado {
-
-}
--- src/main/java/com/departamento/util/AppSettings.java
+++
@@ -1,7 +0,0 @@
-package com.departamento.util;
-
-public class AppSettings {
-
-	public static final String URL_CROSS_ORIGIN = "http://localhost:4200";
-	
-}
