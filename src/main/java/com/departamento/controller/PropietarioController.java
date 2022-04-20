7e960e71360523f827957c09d3dfdca757b97019
Brandoly <brandolmch@gmail.com>
2022-04-20 15:18:42 -0500

    Registrar Propietario

--- src/main/java/com/departamento/controller/PropietarioController.java
+++ src/main/java/com/departamento/controller/PropietarioController.java
@@ -1,5 +1,54 @@
 package com.departamento.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.departamento.entity.Propietario;
+import com.departamento.service.PropietarioService;
+
+@RestController
+@RequestMapping("/rest/propietario")
 public class PropietarioController {
+	
+	@Autowired
+	private PropietarioService service;
+
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Propietario>> listaPropietario(){
+		List<Propietario> lista = service.listaPropietario();
+		return ResponseEntity.ok(lista);
+	}
+	
+	
+	
+	@PostMapping
+	@ResponseBody
+	public  ResponseEntity<Map<String, Object>> GuardarPropietario(@RequestBody Propietario obj){
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Propietario objSalida = service.GuardarPropietario(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", "Comprueve que todos los campos esten llenados.");
+			}else {
+				salida.put("mensaje", "Propietario registrado.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "El residente con el ID " + obj.getIdresidente() + " no existe");
+		}
+		return ResponseEntity.ok(salida);
+	}
 
 }
---
+++ src/main/java/com/departamento/entity/Propietario.java
@@ -0,0 +1,40 @@
+package com.departamento.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "propietario")
+public class Propietario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idpropietario;
+	private int idresidente;
+	//rivate int idresidente;
+	/*@ManyToOne
+	@JoinColumn(name = "idResidente")*/
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	private Date fechaReg;
+	private int activo;
+}
