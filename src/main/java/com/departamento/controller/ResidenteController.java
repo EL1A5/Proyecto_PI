12123422741ca828435060d9578580469c0e3008
Brandoly <brandolmch@gmail.com>
2022-04-25 17:08:01 -0500

    .

--- src/main/java/com/departamento/controller/ResidenteController.java
+++ src/main/java/com/departamento/controller/ResidenteController.java
@@ -8,8 +8,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +30,9 @@ import com.departamento.service.ResidenteService;
 
 @Controller
 @RequestMapping("/views/Residente/")
-
+/*
 @RestController
-@RequestMapping("/rest/residentes")
+@RequestMapping("/rest/residentes")*/
 @CrossOrigin(origins = "http://localhost:4200")
 
 public class ResidenteController {
@@ -87,7 +91,7 @@ public class ResidenteController {
 		System.out.println("Residente eliminado exitosamente");
 		
 		return "redirect:/views/Residente/";
-
+	}
     @PostMapping
 	@ResponseBody
 	public ResponseEntity<HashMap<String, Object>> registraResidente(@RequestBody Residente obj) {
