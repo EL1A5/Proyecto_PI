b77a467570ff9a9a8f6b8d1e49bfe5032ce777b3
Fran Guido Felix Gomez <ffelixg@ulasalle.edu.pe>
2022-04-27 17:55:32 -0500

    commit 2

--- src/main/java/com/departamento/service/MascotaServiceImpl.java
+++ src/main/java/com/departamento/service/MascotaServiceImpl.java
@@ -1,20 +1,45 @@
 package com.departamento.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.departamento.entity.Mascota;
+
 import com.departamento.repository.MascotaRepository;
+
+
 @Service
-public class MascotaServiceImpl  implements MascotaService{
+public class MascotaServiceImpl implements MascotaService {
+
 	@Autowired
-	private MascotaRepository repository;
-	
+	private MascotaRepository repositorio;
+
 	@Override
-	public Mascota insertaMascota(Mascota obj) {
-		
-		return repository.save(obj);
+	public Mascota insertaActualizaMascota(Mascota obj) {
+		// TODO Auto-generated method stub
+		return repositorio.save(obj);
 	}
+
+	@Override
+	public List<Mascota> listarMascota() {
+		// TODO Auto-generated method stub
+		return repositorio.findAll();
+	}
+
+	@Override
+	public Mascota buscarPorId(int id) {
+		// TODO Auto-generated method stub
+		return repositorio.findById(id).orElse(null);
+	}
+
+	@Override
+	public void eliminar(int id) {
+		 repositorio.deleteById(id);
+		
+	}
+	
 
 }
 
