c9ff81de7c78b1a987abdd5fd3676177cfddac6a
HernanCB <hernancardenascb@gmail.com>
2022-07-02 23:39:14 -0500

    Cambio 4

--- src/main/java/com/departamento/entity/Insidencia.java
+++ src/main/java/com/departamento/entity/Insidencia.java
@@ -2,6 +2,7 @@ package com.departamento.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,7 +20,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "insidencia")
+@Table(name = "incidencia")
 @Getter
 @Setter
 @ToString
@@ -32,7 +33,7 @@ public class Insidencia {
 	private int idincidencia ;
 	
 	@ManyToOne
-	@JoinColumn(name = "idusuario")
+	@JoinColumn(name = "iduser")
 	private Usuario usuario;
 	
 	@ManyToOne
@@ -45,14 +46,14 @@ public class Insidencia {
 	private Departamento departamento;
 	
 	private String tipo ; 
+	
+	@Column(length = 1200)
 	private String descripcion ;
 	private int estado;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date fechareg ;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date fechaatencion ;
 	
 	
 	
